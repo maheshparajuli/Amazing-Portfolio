1fb731ee06af4331b22ba52f3b6f72aefc3fcad4
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-13 23:21:42 +0545

    added device model file in models folder

---
+++ lib/constants/data.dart
@@ -0,0 +1,8 @@
+
+import 'package:device_frame/device_frame.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application/models/device_model.dart';
+
+List<DeviceModel> devices [
+      DeviceModel(device:Devices.android.onePlus8Pro, data:Icons.android),
+];
