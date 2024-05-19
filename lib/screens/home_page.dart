da182fab5e60400e82e74eb384296376db70cece
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-19 17:25:21 +0545

    some changes done in homepage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:custom_button_builder/custom_button_builder.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_application/constants/data.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:device_frame/device_frame.dart';
@@ -18,26 +20,52 @@ class HomePage extends StatelessWidget {
                   begin: Alignment.topLeft,
                   colors: [Colors.blue, Colors.black45])),
         ),
-        SvgPicture.asset("assets/images/blue_cloud.svg",
-        height: size.height,
-        fit: BoxFit.cover,),
-        Row(
+        SvgPicture.asset(
+          "assets/images/blue_cloud.svg",
+          height: size.height,
+          fit: BoxFit.cover,
+        ),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            DeviceFrame(
-                device: Devices.ios.iPhone13ProMax,
-                screen: Container(
-                  color: Colors.red,
-                  child: const Center(
-                    child: Text(
-                      "Hello! Beautiful Souls",
-                      style: TextStyle(
-                        color: Colors.pink,
-                        fontSize: 30.0,
-                      ),
-                    ),
-                  ),
-                ))
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: size.height - 100,
+                  child: DeviceFrame(
+                      device: Devices.ios.iPhone13ProMax,
+                      screen: Container(
+                        color: Colors.red,
+                        child: const Center(
+                          child: Text(
+                            "Hello! Beautiful Souls",
+                            style: TextStyle(
+                              color: Colors.pink,
+                              fontSize: 30.0,
+                            ),
+                          ),
+                        ),
+                      )),
+                ),
+                Row(
+                  children: [
+                    ...List.generate(
+                        devices.length,
+                        (index) => CustomButton(
+                              onPressed: () {},
+                              isThreeD: true,
+                              child: Center(
+                                child: Icon(devices[index].data),
+                              ),
+                            ))
+                  ],
+                )
+              ],
+            ),
           ],
         ),
       ],
