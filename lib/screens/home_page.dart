692a8d521eed204b1cd46aa3613dbd5213e4508e
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-19 22:59:16 +0545

    homepage done

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,63 +12,77 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  colors: [Colors.blue, Colors.black45])),
-        ),
-        SvgPicture.asset(
-          "assets/images/blue_cloud.svg",
-          height: size.height,
-          fit: BoxFit.cover,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: size.height - 100,
-                  child: DeviceFrame(
-                      device: Devices.ios.iPhone13ProMax,
-                      screen: Container(
-                        color: Colors.red,
-                        child: const Center(
-                          child: Text(
-                            "Hello! Beautiful Souls",
-                            style: TextStyle(
-                              color: Colors.pink,
-                              fontSize: 30.0,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    colors: [Colors.blue, Colors.black45])),
+          ),
+          SvgPicture.asset(
+            "assets/images/blue_cloud.svg",
+            height: size.height,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: size.height - 100,
+                    child: DeviceFrame(
+                        device: Devices.ios.iPhone13ProMax,
+                        screen: Container(
+                          color: Colors.red,
+                          child: const Center(
+                            child: Text(
+                              "Hello! Beautiful Souls",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30.0,
+                              ),
                             ),
                           ),
-                        ),
-                      )),
-                ),
-                Row(
-                  children: [
-                    ...List.generate(
-                        devices.length,
-                        (index) => CustomButton(
-                              onPressed: () {},
-                              isThreeD: true,
-                              child: Center(
-                                child: Icon(devices[index].data),
+                        )),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ...List.generate(
+                      devices.length,
+                      (index) => CustomButton(
+                            height: 40,
+                            width: 40,
+                            borderRadius: 40,
+                            backgroundColor: Colors.black,
+                            onPressed: () {},
+                            isThreeD: true,
+                            animate: true,
+                            shadowColor: Colors.pink,
+                            child: Center(
+                              child: Icon(
+                                devices[index].data,
+                                color: Colors.white,
                               ),
-                            ))
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      ],
-    ));
+                            ),
+                          ))
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
