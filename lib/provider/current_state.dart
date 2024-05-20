d1ba88cde7e8b049322dce4effd8e6a16e737097
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-20 23:37:39 +0545

    made class CurrentState

--- lib/provider/current_state.dart
+++ lib/provider/current_state.dart
@@ -3,10 +3,12 @@ import "package:flutter/material.dart";
 import 'package:device_frame/device_frame.dart';
 
 class CurrentState extends ChangeNotifier {
-  DeviceInfo currentDevice = Devices.ios.iPhone13;
+  DeviceInfo currentDevice = Devices.ios.iPhone13ProMax;
 
   void changeSelectedDevice(DeviceInfo device) {
     currentDevice = device;
     notifyListeners();
   }
 }
+
+
