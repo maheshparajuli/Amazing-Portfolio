864d4bd04369440aff73bf32f524cf0245f56c9b
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-20 23:35:37 +0545

    changes done on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/provider/current_state.dart';
 import 'package:flutter_application/screens/home_page.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_application/provider/current_state.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -15,12 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context)=>CurrentState())
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: {
+      providers: [ChangeNotifierProvider(create: (context) => CurrentState())],
+      child: MaterialApp(debugShowCheckedModeBanner: false, routes: {
         "/": (context) => const HomePage(),
       }),
     );
