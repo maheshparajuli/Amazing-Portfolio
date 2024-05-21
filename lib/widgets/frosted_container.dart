0aea3ea9e77536b3ce01c965471d17c088d9eb56
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-21 23:57:28 +0545

    added frosted conatainer in widgets folder

---
+++ lib/widgets/frosted_container.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class FrostedContainer extends StatelessWidget {
+  const FrostedContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Stack(
+          children: [
+            BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 8,
+                  sigmaY: 8,
+                ),
+                child: Container(
+                  width: 130,
+                  height: 220,
+                )),
+            Container(
+              height: 220,
+              width: 130,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                Colors.white.withOpacity(0.5),
+                Colors.white.withOpacity(0.3)
+              ])),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
