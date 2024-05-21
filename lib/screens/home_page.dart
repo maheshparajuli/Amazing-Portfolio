3fdfd69a6a4e0d92a29314f033b85f919fdb9ea9
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-21 23:58:11 +0545

    done some changes in homepage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:custom_button_builder/custom_button_builder.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_application/constants/data.dart';
@@ -37,6 +39,11 @@ class HomePage extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  Column(
+                    children: [
+                
+                    ],
+                  ),
                   SizedBox(
                     height: size.height - 100,
                     child: Consumer<CurrentState>(
