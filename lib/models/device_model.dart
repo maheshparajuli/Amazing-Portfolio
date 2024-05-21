97d9f81b4a30f7e15cc9bb1480dd1ae85ea96820
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-21 20:36:44 +0545

    changes done

--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -2,8 +2,8 @@ import "package:device_frame/device_frame.dart";
 import "package:flutter/material.dart";
 
 class DeviceModel {
-  DeviceInfo? device;
-  IconData? data;
+  DeviceInfo device;
+  IconData data;
 
   DeviceModel({required this.device, required this.data});
 }
