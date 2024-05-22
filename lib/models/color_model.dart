340fcee64031d2dc3aca86f3a5888fa05d6962d8
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-22 23:28:42 +0545

    added color_model.dart file

---
+++ lib/models/color_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorModel {
+  String svgPath;
+  Gradient gradient;
+  Color color;
+
+  ColorModel(
+      {required this.svgPath, required this.gradient, required this.color});
+}
