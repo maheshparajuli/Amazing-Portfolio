8bed5482cc018890e3ceaa1622568bca9adccfcd
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-22 23:27:50 +0545

    updated data.dart

--- lib/constants/data.dart
+++ lib/constants/data.dart
@@ -1,12 +1,68 @@
-
 import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application/models/device_model.dart';
-
+import 'package:flutter_application/models/color_model.dart';
 
 List<DeviceModel> devices = [
-      DeviceModel(device:Devices.android.onePlus8Pro, data:Icons.android),
-      DeviceModel(device:Devices.ios.iPhone13ProMax, data:Icons.phone_iphone),
-      DeviceModel(device:Devices.ios.iPad, data:Icons.tablet),
+  DeviceModel(device: Devices.android.onePlus8Pro, data: Icons.android),
+  DeviceModel(device: Devices.ios.iPhone13ProMax, data: Icons.phone_iphone),
+  DeviceModel(device: Devices.ios.iPad, data: Icons.tablet),
+];
 
+List<ColorModel> colorPalette = [
+  ColorModel(
+    svgPath: "assets/images/blue_cloud.svg",
+    color: Colors.yellowAccent,
+    gradient: const LinearGradient(
+      begin: Alignment.bottomLeft,
+      // transform: Grad,
+      end: Alignment.topRight,
+      colors: [Colors.yellowAccent, Colors.deepOrange],
+    ),
+  ),
+  ColorModel(
+    svgPath: "assets/images/blue_cloud.svg",
+    color: Colors.blue,
+    gradient: const LinearGradient(
+      begin: Alignment.topLeft,
+      colors: [Colors.blue, Colors.black45],
+    ),
+  ),
+  ColorModel(
+      svgPath: "assets/images/blue_cloud.svg",
+      color: const Color(0xff00d6ca),
+      gradient: const LinearGradient(
+        colors: [Color(0xff00ebd5), Color(0xff293474)],
+        stops: [0, 1],
+        begin: Alignment.bottomRight,
+        end: Alignment.topLeft,
+      )),
+  ColorModel(
+    svgPath: "assets/images/blue_cloud.svg",
+    color: const Color(0xff123cd1),
+    gradient: const LinearGradient(
+      begin: Alignment.topLeft,
+      end: Alignment(-0.31, 0.95),
+      colors: [Color(0xFF1042F4), Color(0x00203EA6)],
+    ),
+  ),
+  ColorModel(
+    svgPath: "assets/images/blue_cloud.svg",
+    color: Colors.purple,
+    gradient: const LinearGradient(
+      colors: [Color(0xffc95edb), Colors.black12],
+      stops: [0, 1],
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+    ),
+  ),
+  ColorModel(
+    svgPath: "assets/images/blue_cloud.svg",
+    color: const Color(0xfff35a32),
+    gradient: const LinearGradient(
+      begin: Alignment.bottomLeft,
+      end: Alignment.topRight,
+      colors: [Colors.indigo, Colors.deepOrange], // Adjust colors as needed
+    ),
+  ),
 ];
