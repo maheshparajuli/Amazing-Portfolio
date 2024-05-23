7191e5b1ce4d0cf645de803d992fca792d75504c
maheshparajuli <maheshparajuli717@gmail.com>
2024-05-23 23:43:02 +0545

    updated current state

--- lib/provider/current_state.dart
+++ lib/provider/current_state.dart
@@ -4,9 +4,14 @@ import 'package:device_frame/device_frame.dart';
 
 class CurrentState extends ChangeNotifier {
   DeviceInfo currentDevice = Devices.ios.iPhone13ProMax;
-
+  int selectedwala = 2;
   void changeSelectedDevice(DeviceInfo device) {
     currentDevice = device;
+    notifyListeners();
+  }
+
+  void changeGradient(int index) {
+    selectedwala = index;
     notifyListeners();
   }
 }
